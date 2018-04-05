a61eed22f1122178bf968a14d16e8e3148c844ae
Muhammet Öztürk <ozturkmuhammeet@gmail.com>
2018-04-06 00:31:53 +0300

    assets eklendi

---
+++ resources/views/index.blade.php
@@ -0,0 +1,435 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- basic page needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>Samsun Bilgi Güvenliği Zirvesi</title>
+	<meta name="description" content="">  
+	<meta name="author" content="">
+
+   <!-- mobile specific metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+ 	<!-- CSS
+   ================================================== -->
+	 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+   <link rel="stylesheet" href="assets/css/bootstrap.min.css">   
+   <link rel="stylesheet" href="assets/css/base.css">
+   <link rel="stylesheet" href="assets/css/vendor.css">  
+   <link rel="stylesheet" href="assets/css/main.css"> 
+   <link rel="stylesheet" href="assets/css/countdown.css"> 
+
+   <!-- script
+   ================================================== -->
+	<script src="assets/js/modernizr.js"></script>
+	<script src="assets/js/pace.min.js"></script>
+
+   <!-- favicons
+	================================================== -->
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+</head>
+
+<body id="top">
+
+	<!-- header 
+================================================== -->
+   <header> 
+
+   	<div class="header-logo">
+	      <a href="index.html">Samsun Bilgi Güvenliği Zirvesi</a>
+	   </div> 
+
+		<a id="header-menu-trigger" href="#0">
+		 	<span class="header-menu-text">Menü</span>
+		  	<span class="header-menu-icon"></span>
+		</a> 
+
+		<nav id="menu-nav-wrap">
+
+			<a href="#0" class="close-button" title="close"><span>Kapat</span></a>	
+
+	   	<h3>Samsun Bilgi Güvenliği Zirvesi</h3>
+
+			<ul class="nav-list">
+				<li class="current"><a class="smoothscroll" href="#home" title="">Başlangıç</a></li>
+				<li><a class="smoothscroll" href="#about" title="">Hakkında</a></li>
+				<li><a class="smoothscroll" href="#speakers" title="">Konuşmacılar</a></li>
+				<li><a class="smoothscroll" href="#program" title="">İçerik</a></li>
+				<li><a class="smoothscroll" href="#sponsors" title="">Sponsorlar</a></li>
+				<li><a class="smoothscroll" href="#register" title="">Kayıt</a></li>						
+			</ul>		
+			<ul class="header-social-list">
+	         <li>
+	         	<a href="#"><i class="fa fa-facebook-square"></i></a>
+	         </li>
+	         <li>
+	         	<a href="#"><i class="fa fa-twitter"></i></a>
+	         </li>
+	         <li>
+	         	<a href="#"><i class="fa fa-instagram"></i></a>
+	         </li>
+			<li><a href=""><i class="fa fa-github"></i></a></li>        
+	      </ul>		
+		</nav>  <!-- end #menu-nav-wrap -->
+	</header> <!-- end header -->  
+
+
+   <!-- home
+   ================================================== -->
+   
+	 
+	 <section id="home">
+
+   	<div class="overlay"></div>
+
+   	<div class="home-content-table">	
+		   <div class="home-content-tablecell">
+		   	<div class="row">
+		   		<div class="col-twelve">		   			
+			  		
+			  				<h3 class="animate-intro">Samsun</h3>
+				  			<h1 class="animate-intro">
+							Bilgi Güvenliği Zirvesi
+				  			</h1>
+								<h1 class="animate-intro" style="font-size: 2rem">
+									<i class="fa fa-map-marker"></i>
+									Mühendislik Fakültesi Konferans Salonu
+								</h1>	
+								<div class="container"></div>					
+
+			  		</div> <!-- end col-twelve --> 
+		   	</div> <!-- end row --> 
+		   </div> <!-- end home-content-tablecell --> 		   
+		</div> <!-- end home-content-table -->
+
+		<ul class="home-social-list">
+	      <li class="animate-intro">
+	        	<a href="https://facebook.com/omusiber"><i class="fa fa-facebook-square"></i></a>
+	      </li>
+	      <li class="animate-intro">
+	        	<a href="https://twitter.com/omusiber"><i class="fa fa-twitter"></i></a>
+	      </li>
+	      <li class="animate-intro">
+	        	<a href="https://instagram.com/omusiber"><i class="fa fa-instagram"></i></a>
+	      </li>
+	      <li class="animate-intro">
+	        	<a href="#"><i class="fa fa-linkedin"></i></a>
+	      </li>	      
+	   </ul> <!-- end home-social-list -->	
+
+		<div class="scrolldown">
+			<a href="#about" class="scroll-icon smoothscroll">		
+		   	Kaydır		   	
+		   	<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+			</a>
+		</div>			
+   
+   </section> <!-- end home -->
+
+
+   <!-- about
+   ================================================== -->
+   <section id="about">
+
+   	<div class="container">
+				<div class="row">
+					<h3 class="lead animate-this title">Hakkında</h3>
+	   			<p class="animate-this">Samsun ili, bilgi teknolojileri alanındaki kurumsal yapısı itibariyle Karadeniz'in tamamı ve İç Anadolu Bölgesinin bir kısmı olmak üzere toplamda 14 ilin merkezi konumundadır.</p>
+          <p class="animate-this">Ondokuz Mayıs Üniversitesi Siber Güvenlik Topluluğu, başlatacağı ve gelenekselleştireceği bu etkinlikle, Samsun'un ülkemizdeki stratejik konumuna dikkat çekmek ve bilgi güvenliği alanıyla Samsun ilini özdeşleştirmeyi hedeflemektedir.</p>
+				</div>
+   	</div> <!-- end about-wrap  -->
+
+   </section> <!-- end about -->
+
+
+  <!-- speakers
+  ================================================== -->
+  <section id="speakers">
+    <h3 class="lead animate-this title">Konuşmacılar</h3>
+    <div class="grid container">
+      <div class="row">
+        <div class="col-md-3">
+          <figure class="effect-sadie">
+            <img src="assets/images/speakers/caner.jpeg" alt="Caner Köroğlu"/>
+            <figcaption>
+              <h2>Caner <span>Köroğlu</span></h2>
+              <p>
+                <a class="social" href="#">
+                  <i class="fa fa-twitter-square"></i>
+                </a>
+                <a class="social" href="#">
+                  <i class="fa fa-linkedin-square"></i>
+                </a>
+              </p>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-md-3">
+          <figure class="effect-sadie">
+            <img src="assets/images/speakers/kursat.jpeg" alt="Kürşat Oğuzhan Akıncı"/>
+            <figcaption>
+              <h2>Kürşat Oğuzhan <span>Akıncı</span></h2>
+              <p>
+                <a class="social" href="#">
+                  <i class="fa fa-twitter-square"></i>
+                </a>
+                <a class="social" href="#">
+                  <i class="fa fa-linkedin-square"></i>
+                </a>
+              </p>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-md-3">
+          <figure class="effect-sadie">
+            <img src="assets/images/speakers/gokay.jpeg" alt="Gökay Bekşen"/>
+            <figcaption>
+              <h2>Gökay <span>Bekşen</span></h2>
+              <p>
+                <a class="social" href="#">
+                  <i class="fa fa-twitter-square"></i>
+                </a>
+                <a class="social" href="#">
+                  <i class="fa fa-linkedin-square"></i>
+                </a>
+              </p>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-md-3">
+          <figure class="effect-sadie">
+            <img src="assets/images/speakers/mustafa.jpeg" alt="Mustafa Altınkaynak"/>
+            <figcaption>
+              <h2>Mustafa <span>Altınkaynak</span></h2>
+              <p>
+                <a class="social" href="#">
+                  <i class="fa fa-twitter-square"></i>
+                </a>
+                <a class="social" href="#">
+                  <i class="fa fa-linkedin-square"></i>
+                </a>
+              </p>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+      </div>
+    </div>
+  </section>
+
+   <!-- program
+   ================================================== -->
+
+	<section id="program">
+    <h3 class="lead animate-this title">İÇERİK</h3>
+    <div class="grid">
+			<p>Etkinliğe ait içerik yakında eklenecektir.</p>
+    </div>
+  </section>
+
+   <!-- sponsors
+   ================================================== -->
+   <section id="sponsors">
+    <h3 class="lead animate-this title">Sponsorlar</h3>
+    <div class="grid">
+      	<div class="row sponsor main-sponsor">
+	  	  	<h3>Ana Sponsor</h3>
+					<img src="https://media.licdn.com/dms/image/C5603AQGW8Qb11Zmvjg/profile-displayphoto-shrink_200_200/0?e=1526220000&v=alpha&t=pW6sIwYYARajSA09ywX0B6ymGqKASLvzcGXTpSIXHuM" alt="img02"/>
+    		</div>
+				<div class="row sponsor gold-sponsor">
+					<h3>Altın Sponsor</h3>
+					<img src="https://media.licdn.com/dms/image/C5603AQGW8Qb11Zmvjg/profile-displayphoto-shrink_200_200/0?e=1526220000&v=alpha&t=pW6sIwYYARajSA09ywX0B6ymGqKASLvzcGXTpSIXHuM" alt="img02"/>
+					<img src="https://media.licdn.com/dms/image/C5603AQGW8Qb11Zmvjg/profile-displayphoto-shrink_200_200/0?e=1526220000&v=alpha&t=pW6sIwYYARajSA09ywX0B6ymGqKASLvzcGXTpSIXHuM" alt="img02"/>
+				</div>
+      	<div class="row sponsor main-sponsor">
+	  	  	<h3>Gümüş Sponsor</h3>
+					<img src="https://media.licdn.com/dms/image/C5603AQGW8Qb11Zmvjg/profile-displayphoto-shrink_200_200/0?e=1526220000&v=alpha&t=pW6sIwYYARajSA09ywX0B6ymGqKASLvzcGXTpSIXHuM" alt="img02"/>
+    		</div>
+      	<div class="row sponsor main-sponsor">
+	  	  	<h3>Bronz Sponsor</h3>
+					<img src="https://media.licdn.com/dms/image/C5603AQGW8Qb11Zmvjg/profile-displayphoto-shrink_200_200/0?e=1526220000&v=alpha&t=pW6sIwYYARajSA09ywX0B6ymGqKASLvzcGXTpSIXHuM" alt="img02"/>
+    		</div>
+				<div class="row supporters">
+					<h3>Destekleyenler</h3>
+					<img src="assets/images/supporters/blackbox.png" alt="BlackBox Security"/>
+					<img src="assets/images/supporters/cypm.png" alt="Canyoupwnme"/>
+					<img src="assets/images/supporters/arkakapi.png" alt="Arka Kapı"/>
+					<img src="assets/images/supporters/cypm.png" alt="img02"/>
+					<img src="assets/images/supporters/blackbox.png" alt="img02"/>
+					<img src="assets/images/supporters/cypm.png" alt="img02"/>
+				</div>
+    </div>
+  </section>
+
+	<!-- register
+   ================================================== -->
+   <section id="register">
+    <div class="overlay"></div>
+		<div class="container">
+			<div class="row section-intro with-bottom-sep animate-this">
+   		<div class="col-twelve">
+   			<h3>Kayıt Ol</h3>
+
+   			<p class="lead">Etkinliğe katılmak için kayıt olmanız gerekmektedir.</p>
+   		</div> 
+   	</div> <!-- end section-intro -->
+
+   	<div class="row contact-content">
+
+   		<div class="col-seven tab-full animate-this">
+
+            <!-- form -->
+            <form name="contactForm" id="contactForm" method="post">     			
+
+               <div class="form-field">
+ 					   <input name="name" type="text" id="contactName" placeholder="Ad Soyad" value="" minlength="2" required="">
+               </div>
+
+                 		<div class="form-field">
+                 			<input name="email" type="email" id="contactEmail" placeholder="Email Adresi" value="" required="">
+                 		</div>		           
+	            		<div class="form-field">
+	            			<input name="phone" type="text" id="contactPhone" placeholder="Telefon" value="">
+	                  </div>	
+                                         
+               <div class="form-field">
+	              	<input type="text" name="corporation" id="contactCorp" placeholder="Kurum / Okul" required=""/>
+	            </div> 
+
+               <div class="form-field">
+                  <button class="submitform">Katıl</button>
+
+                  <div id="submit-loader">
+                     <div class="text-loader">Gönderiliyor...</div>                             
+       			      <div class="s-loader">
+							  	<div class="bounce1"></div>
+							  	<div class="bounce2"></div>
+							  	<div class="bounce3"></div>
+							</div>
+						</div>
+               </div>
+
+      		</form> <!-- end form -->
+
+            <!-- contact-warning -->
+            <div id="message-warning"></div> 
+
+            <!-- contact-success -->
+      		<div id="message-success">
+               <i class="fa fa-check"></i>İlginiz için teşekkür ederiz! En kısa zamanda mail ile döneceğiz.<br>
+      		</div>
+
+         </div> <!-- end col-seven --> 
+
+         <div class="col-four tab-full contact-info end animate-this">
+
+         	<h5>Düzenleyen Bilgileri</h5>
+
+         	<div class="cinfo">
+	   			<h6>Bize ulaşın</h6>
+	   			<p>
+	            	Yaşam Merkezi 2. Kat<br>
+	            	Kurupelit Kampüsü, OMU<br>
+	            	Samsun
+	            </p>
+	   		</div> <!-- end cinfo -->
+
+	   		<div class="cinfo social">
+	   			<h6>Email</h6>
+	   			<p>
+	   				siber@omu.edu.tr	     
+				   </p>
+					 <h6>Twitter</h6>
+	   			<p>
+	   				<a target="_blank" href="https://twitter.com/omusiber">@omusiber</a>	     
+				   </p>
+					 <h6>Instagram</h6>
+	   			<p>
+	   				<a target="_blank" href="https://instagram.com/omusiber">omusiber</a>	     
+				   </p>
+	   		</div> <!-- end cinfo -->
+
+         </div> <!-- end cinfo --> 
+
+   	</div> <!-- end row contact-content -->
+		</div>
+		
+	</section> <!-- end contact -->
+
+
+	<!-- footer
+   ================================================== -->
+	<footer>
+     	<div class="footer-main">
+
+   		<div class="row">  
+
+	      	<div class="col-five tab-full footer-about">       
+
+	            <h4 class="h05">Samsun Bilgi Güvenliği Zirvesi</h4>
+
+	            <p>
+								Bilgi Güvenliği Zirvesi, Ondokuz Mayıs Üniversitesi Siber Güvenlik Topluluğu tarafından her sene düzenlenen 
+								bir etkinliktir.
+							</p>	            
+
+		      </div> <!-- end footer-about -->
+
+	      	<div class="col-three tab-full footer-social">
+
+	      		<h4 class="h05">Takip edin.</h4>
+
+	      		<ul class="list-links">
+	      			<li><a href="#">Facebook</a></li>
+						<li><a href="#">Twitter</a></li>
+						<li><a href="#">Instagram</a></li>					
+					</ul>
+
+	      	</div> <!-- end footer-social -->       	    
+
+	      </div> <!-- end row -->
+
+   	</div> <!-- end footer-main -->
+
+   	<div class="footer-bottom">
+
+      	<div class="row">
+
+      		<div class="col-twelve">
+	      		<div class="copyright">
+		         	<span>© Copyright 2018.</span> 
+		         	<span>Design by <a href="http://www.styleshout.com/">styleshout</a></span>		         	
+		         </div>		               
+	      	</div>
+
+      	</div>   	
+
+      </div> <!-- end footer-bottom -->
+
+      <div id="go-top">
+		   <a class="smoothscroll" title="Back to Top" href="#top">
+		   	<i class="fa fa-long-arrow-up" aria-hidden="true"></i>
+		   </a>
+		</div>		
+   </footer>
+
+   <div id="preloader"> 
+    	<div id="loader"></div>
+   </div> 
+
+   <!-- Java Script
+   ================================================== --> 
+   <script src="assets/js/jquery-2.1.3.min.js"></script>
+   <script src="assets/js/plugins.js"></script>
+   <script src="assets/js/main.js"></script>
+   <script src="assets/js/countdown.js"></script>
+
+</body>
+
+</html>
