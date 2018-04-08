9f0ee9f0d56eb1cb3601852dabcb38e847a68fad
Muhammet Öztürk <ozturkmuhammeet@gmail.com>
2018-04-08 23:07:26 +0300

    Eklemeler

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -33,6 +33,7 @@
             img{
                 display: block;
                 margin: 0 auto;
+                width: 364px;
             }
             div.row img{
                 width: 100px;
